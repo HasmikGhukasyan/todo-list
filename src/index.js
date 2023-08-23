323ecbeca8b936f4833df01016577a97a3ea30dc
Sokrat <sokrat.upwork@gmail.com>
2023-08-23 21:07:01 +0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+import Header from "./components/Header"
+import Search from "./components/Search"
+import TodoList from "./components/TodoList"
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Search />
+      <TodoList />
+    </div>
+  )
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>);
