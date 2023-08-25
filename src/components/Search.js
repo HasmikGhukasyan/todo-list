e0f4897a1c69e3f5a9c99928e847784f817d61c6
Sokrat <sokrat.upwork@gmail.com>
2023-08-25 10:17:40 +0400

    testing

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,5 @@
 const Search = () => {
-    return <input type = "text" placeholder="Type text for Search..."/>
+    return <input type = "text" placeholder="Type text for"/>
   }
 
   export default Search;
