c97c37ec6e79c1afb7d8b825bd26d1d292ce85ea
Sokrat <sokrat.upwork@gmail.com>
2023-09-02 19:50:57 +0400

    added functionality

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,15 +1,21 @@
+import { Component } from "react";
 import "./search.css"
 
-const Search = () => {
-    return (
-    <div className="searchcontainer">
-      <input className="searchInput" placeholder="Search..."/>
-      <div className="search-btn-container">
-      <button className="search-btn-done" >Done</button>
-      <button className="search-btn-important">Important</button>
-      <button className="search-btn-all">All</button>
-      </div>
-      </div>
-  )}
 
-  export default Search;
+class Search extends Component {
+  render() {
+    return (
+      <div className="searchcontainer">
+        <input className="searchInput" placeholder="Search..." />
+        <div className="search-btn-container">
+          <button className="search-btn-done" >Done</button>
+          <button className="search-btn-important">Important</button>
+          <button className="search-btn-all">All</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default Search;
--- src/components/ToDoList/ToDoListItem/ToDoListItem.js
+++ src/components/ToDoList/ToDoListItem/ToDoListItem.js
@@ -1,25 +1,53 @@
+import { Component } from "react"
 import "./todoListItem.css"
-import {FaTrash, FaCheck, FaExclamation} from "react-icons/fa6"
+import { FaTrash, FaCheck, FaExclamation } from "react-icons/fa6"
 
-const TodoListItem = ({ text, important }) => {
 
-    const style = {
-        color: important ? "#EF6262" : "blue",
-        fontWeight: important ? "bold" : "unset"
+
+
+class TodoListItem extends Component {
+     state = {
+        isDone: false,
+        isImportant: this.props.important
+    }
+onImportant = () => {
+    this.setState({
+        isImportant: !this.state.isImportant
+    })
+}
+    onDone = () => {
+        this.setState({
+            isDone:!this.state.isDone
+        })
+        
     }
 
-    return (<div className="listItem">
-        <div className="taskContainer">
-            <li className="task" style={style}> {text}</li>
-        </div>
-        <div className="buttonContainer">
-            <button className="done-btn"><FaCheck/  ></button>
-            <button className="important-btn"><FaExclamation/></button>
-            <button className="delete-btn"><FaTrash/></button>
-        </div>
-    </div>
+    render() {
+        const { text } = this.props
+        const {isDone,isImportant} = this.state
 
-    )
+        const textStyle = {
+            color: isDone ? "#aaa": isImportant ? "#EF6262" : "blue",
+            fontWeight: isDone ? "normal" : isImportant ? "bold" : "unset",
+            textDecoration: isDone ? "line-through" : "none",
+
+
+        }
+
+        return (<div className="listItem">
+            <div className="taskContainer" onClick={this.onDone}>
+                <li className="task" style={textStyle}> {text}</li>
+            </div>
+            <div className="buttonContainer">
+                <button className="done-btn" onClick={this.onDone}><FaCheck /></button>
+                <button className="important-btn" onClick={this.onImportant}><FaExclamation /></button>
+                <button className="delete-btn"><FaTrash /></button>
+            </div>
+        </div>
+
+        )
+    }
 }
+
 
 export default TodoListItem;
