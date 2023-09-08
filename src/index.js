8914fa5350797b293433a940d0089bd399ffe6c3
Hasmik Ghukasyan <hasmikghukasyan98@gmail.com>
2023-09-08 12:03:29 +0400

    updated editHandler function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom/client"
-import Header from "./components/Header"
-import Search from "./components/Search"
-import TodoList from "./components/ToDoList"
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import TodoList from "./components/ToDoList";
 import AddItem from "./components/AddItem";
-import "./index.css"
+import "./index.css";
 
 class App extends Component {
   state = {
@@ -12,76 +12,74 @@ class App extends Component {
       { text: "Learn JS", important: true, done: false, id: 1 },
       { text: "Drink tea", important: false, done: false, id: 2 },
       { text: "Learn React", important: true, done: false, id: 3 },
-      { text: "Learn Typescript", important: true, done: false, id: 4 }
+      { text: "Learn Typescript", important: true, done: false, id: 4 },
     ],
-  }
+  };
 
   doneHandler = (id) => {
     this.setState(({ items }) => {
-      const arr = [...items]
-      const idx = arr.findIndex(el => el.id === id)
-      arr[idx].done = !arr[idx].done
+      const arr = [...items];
+      const idx = arr.findIndex((el) => el.id === id);
+      arr[idx].done = !arr[idx].done;
       return {
-        items: arr
-      }
-    })
-  }
+        items: arr,
+      };
+    });
+  };
 
   importantHandler = (id) => {
     this.setState(({ items }) => {
-      const arr = [...items]
-      const idx = arr.findIndex(el => el.id === id)
+      const arr = [...items];
+      const idx = arr.findIndex((el) => el.id === id);
       arr[idx].important = !arr[idx].important;
       return {
-        items: arr
-      }
-    })
-  }
+        items: arr,
+      };
+    });
+  };
 
   AddItemHandler = (text) => {
-    const id = this.state.items.length ? this.state.items[this.state.items.length - 1].id + 1 : 1
+    const id = this.state.items.length
+      ? this.state.items[this.state.items.length - 1].id + 1
+      : 1;
     const newItem = {
       text,
       id,
       important: false,
-    }
-
+    };
 
     this.setState((prevState) => {
       return {
-        items: [...prevState.items, newItem]
-      }
-    })
-  }
+        items: [...prevState.items, newItem],
+      };
+    });
+  };
 
   deleteHandler = (id) => {
     this.setState(({ items }) => {
-      const idx = items.findIndex((el) => el.id === id)
+      const idx = items.findIndex((el) => el.id === id);
 
       return {
-        items: [
-          ...items.slice(0, idx),
-          ...items.slice(idx + 1)]
-      }
-    })
-  }
+        items: [...items.slice(0, idx), ...items.slice(idx + 1)],
+      };
+    });
+  };
 
   editHandler = (id, value) => {
-    this.setState(({ items }) => {
-      const idx = items.findIndex((el) => el.id === id)
-      const arr = [...items]
-      arr[idx].text = value
-      return {
-        items: arr
-      }
-    })
-  }
-
-
-
+    this.setState({
+      items: this.state.items.map((el) => {
+        if (el.id == id) {
+          return {
+            ...el,
+            text: value,
+          };
+        }
+        return el;
+      }),
+    });
+  };
 
   render() {
-
     return (
       <div className="App">
         <Header done={8} important={23} />
@@ -91,12 +89,13 @@ class App extends Component {
             items={this.state.items}
             doneHandler={this.doneHandler}
             importantHandler={this.importantHandler}
-            deleteHandler={this.deleteHandler} editHandler={this.editHandler}
+            deleteHandler={this.deleteHandler}
+            editHandler={this.editHandler}
           />
           <AddItem AddItemHandler={this.AddItemHandler} />
         </div>
       </div>
-    )
+    );
   }
 }
 
